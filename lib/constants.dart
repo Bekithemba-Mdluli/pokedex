c633dd4489322d080a00bd9ee010dbb865d07b47
Bekithemba <putthetrust.gamedev@gmail.com>
2023-02-14 11:55:19 +0200

    Initaial commit

---
+++ lib/constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// Colors
+Color primaryColor = const Color.fromRGBO(220, 10, 45, 0);
+
+Color bug = const Color.fromRGBO(167, 183, 35, 0);
+Color dartk = const Color.fromRGBO(117, 87, 76, 0);
+Color dragon = const Color.fromRGBO(112, 55, 255, 0);
+Color electric = const Color.fromRGBO(249, 207, 48, 0);
+Color fairy = const Color.fromRGBO(230, 158, 172, 0);
+Color fighting = const Color.fromRGBO(193, 34, 57, 0);
+Color fire = const Color.fromRGBO(245, 125, 49, 0);
+Color flying = const Color.fromRGBO(168, 145, 236, 0);
+Color ghost = const Color.fromRGBO(112, 85, 155, 0);
+Color normal = const Color.fromRGBO(170, 166, 127, 0);
+Color grass = const Color.fromRGBO(116, 203, 72, 0);
+Color ground = const Color.fromRGBO(222, 193, 107, 0);
+Color ice = const Color.fromRGBO(154, 214, 223, 0);
+Color poison = const Color.fromRGBO(164, 62, 158, 0);
+Color psychic = const Color.fromRGBO(251, 85, 132, 0);
+Color rock = const Color.fromRGBO(182, 158, 49, 0);
+Color steel = const Color.fromRGBO(183, 185, 208, 0);
+Color water = const Color.fromRGBO(100, 147, 235, 0);
+
+// Neutral
+Color dark = const Color.fromRGBO(29, 29, 29, 0);
+Color medium = const Color.fromRGBO(102, 102, 102, 0);
+Color light = const Color.fromRGBO(224, 224, 224, 0);
+Color background = const Color.fromRGBO(239, 239, 239, 0);
+Color white = const Color.fromRGBO(255, 255, 255, 0);
